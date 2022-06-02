49f213efa1a217b8376913d8c2117ec356c4419b
Alex Chechulov <alex.chechulov@gmail.com>
2022-06-02 13:50:47 +0300

    Adding and preconfiguring charts. Correcting code.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "@progress/kendo-theme-material/dist/all.css";
 import "hammerjs";
 import "./App.css";
-import Line from "./components/charts/line";
+// import Line from "./components/charts/line";
 import { Button,Row,Col,Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Chart,
+  ChartSeries,
+  ChartSeriesItem,
+  ChartValueAxis,
+  ChartValueAxisItem,
+  ChartCategoryAxis,
+  ChartCategoryAxisItem,
+  ChartTitle,
+  ChartLegend,
+} from "@progress/kendo-react-charts";
 
 import SensorData from './sensorData';
 import React from "react";
@@ -13,6 +24,14 @@ import React from "react";
 //     <Line />
 //   </div>
 // </div>
+
+const series = [
+  {
+    data: [19, 9, 20,15,20,33],
+  },
+
+];
+
 class App extends React.Component {
       constructor(){
           super();
@@ -30,6 +49,8 @@ class App extends React.Component {
                         "Влажность почвы",
                         "Влажность листьев",
                         "Кислотность почвы"],
+              lastSelectedSensor:"",
+
 };
           this.handleClick = this.handleClick.bind(this);
       }
@@ -44,8 +65,8 @@ class App extends React.Component {
           {SensorData(e.target.dataset.sensor).value}</div>
           <button className="close">X</button>
         </div>
-    ]
-
+    ],
+    lastSelectedSensor:e.target.dataset.sensor
   });
 }
   render(){
@@ -61,8 +82,33 @@ class App extends React.Component {
         </Col>
         <Col xs="10">
         <Row className="main-window">{this.state.children.map(child => child)}</Row>
-        <Row className="choise-data-window"></Row>
-        <Row className="main-window"></Row>
+        <Row className="choise-data-window">
+
+        </Row>
+
+        <Row className="main-window">
+        <Chart pannable zoomable style={{ height: 452}}>
+          <ChartTitle text="status - last 6 days" />
+          <ChartLegend position="top" orientation="horizontal" />
+          <ChartValueAxis>
+            <ChartValueAxisItem title={{ text: "Job Positions" }} min={0} max={40} />
+          </ChartValueAxis>
+          <ChartCategoryAxis>
+            <ChartCategoryAxisItem categories={["10.03", "11.03", "12.03", "13.03", "14.03", "15.03"]} />
+          </ChartCategoryAxis>
+          <ChartSeries>
+            {series.map((item, idx) => (
+              <ChartSeriesItem
+                key={idx}
+                type="line"
+                tooltip={{ visible: true }}
+                data={item.data}
+                name={this.state.lastSelectedSensor}
+              />
+            ))}
+          </ChartSeries>
+        </Chart>
+        </Row>
         </Col>
       </Row>
   </Container>
